db28318ddf55c67c136d53a3cafa3087a039c073
kenil261615 <68728138+kenil261615@users.noreply.github.com>
2021-12-07 18:03:23 +0530

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -33,7 +33,7 @@ def ratings(bot,update):
 
 
 if __name__ == "__main__":
-    token = os.getenv('5044795332:AAGzM3BKuTP8Ir-F1kmLClPam-iwbpdeyPo')
+    token = os.getenv('TOKEN')
     updater = Updater(token)
     dispatcher = updater.dispatcher
     bot = telegram.Bot(token)
